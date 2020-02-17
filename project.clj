0d340e53599c2fee99e87d875dc203d6d990fc39
Yehonathan Sharvit <viebel@gmail.com>
2020-02-17 16:44:55 +0200

    version 7.9.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "7.8.3"
+(defproject klipse "7.9.4"
   :description "Embeddable multi-language WEB REPL"
   :resource-paths ["scripts" "src" "resources" "target"]
   :clean-targets ^{:protect false} ["resources/public/dev/js"]
